5cd471c983550e8e38f1aae486174ac0097a107f
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-08-10 15:45:50 +0530

    Players Runs by a Given Team Function Implemented

--- read.py
+++ read.py
@@ -28,3 +28,33 @@ def get_total_runs_by_team():
                 total_runs_by_team[playing_team] = total_run
         
     return total_runs_by_team
+
+
+def get_player_runs_by_team(team):
+
+    # Dictionary to Store players Runs by a given team.
+    player_runs_by_team = dict()
+
+    with open(deliveries_csv) as file:
+
+        reader = csv.reader(file)
+        
+        # First Row needs to be removed
+        first_row = next(reader)
+        
+        for row in reader:
+
+            playing_team = row[2]
+            player_run = int(row[-6])
+            player_name = row[6]
+
+            if playing_team == team:
+                
+                if player_name in player_runs_by_team.keys():
+                    player_runs_by_team[player_name] += player_run
+                else:
+                    player_runs_by_team[player_name] = player_run
+            else:
+                pass
+
+    return player_runs_by_team
