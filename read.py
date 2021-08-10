d433842a3e7720956bec1fa2094cc60552943814
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-08-10 16:03:00 +0530

    Added Comments in read.py

--- read.py
+++ read.py
@@ -1,5 +1,4 @@
 import csv
-from os import read
 
 # Data Files.
 deliveries_csv = './data/deliveries.csv'
@@ -17,11 +16,13 @@ def get_total_runs_by_team():
         # First Row Needs to avoided.
         first_row = next(reader)
 
+        # Iterating Through all rows
         for row in reader:
 
-            playing_team = row[2]
-            total_run = row[-4]
+            playing_team = row[2]     # get team name
+            total_run = row[-4]       # get total run on the ball
             
+            # IF playing team is in dictionary.
             if playing_team in total_runs_by_team.keys():
                 total_runs_by_team[playing_team] += total_run
             else:
@@ -42,14 +43,17 @@ def get_player_runs_by_team(team):
         # First Row needs to be removed
         first_row = next(reader)
         
+        #Iterating Throw All rows
         for row in reader:
 
-            playing_team = row[2]
-            player_run = int(row[-6])
-            player_name = row[6]
+            playing_team = row[2]        # Get Player Team
+            player_run = int(row[-6])    # Get Run Made by Player on The Ball
+            player_name = row[6]         # Get Player Name
 
+            # if team is same as the playing team
             if playing_team == team:
                 
+                # Check if player name in the dictionary
                 if player_name in player_runs_by_team.keys():
                     player_runs_by_team[player_name] += player_run
                 else:
