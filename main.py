91245cb1e32b7914996826713d69ad6a239e979c
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-08-12 23:48:20 +0530

    All utility plot function defined
    Functions are not completed yet

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import read
+
+# function to plot total runs scored by team.
+def plot_total_run_by_team():
+    pass
+
+# function to plot total run scored by batsman for a team
+def plot_batsman_run_by_team():
+    pass
+
+# function to plot count of foreign umprie by origin country
+def plot_foreign_umprire_analysis():
+    pass
+
+# function to plot matches played by team
+def plot_matches_played_by_team():
+    pass
+
+# function to plot matched played by season
+def plot_matches_played_by_season():
+    pass
+
+# Main function...
 def main():
     pass
 
