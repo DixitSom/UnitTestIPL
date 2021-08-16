b5d5c482800255a055709a2c17265ba2b7034fdb
Rohan Dixit <rohank.dixit9625@gmail.com>
2021-08-16 14:30:51 +0530

    Test Case for player runs by team Implemented

--- test_read.py
+++ test_read.py
@@ -1,7 +1,7 @@
 import unittest
-from unittest import result
 
 import read
+
 
 class TestIPL(unittest.TestCase):
 
@@ -10,6 +10,14 @@ class TestIPL(unittest.TestCase):
 
         self.assertEqual(result['Royal Challengers Bangalore'], 23436)
         self.assertEqual(result['Chennai Super Kings'], 20899)
+
+    def test_get_player_runs_by_team(self):
+        result = read.get_player_runs_by_team('Chennai Super Kings')
+
+        self.assertEqual(result['MS Dhoni'], 2986)
+        self.assertEqual(result['SK Raina'], 3707)
+        self.assertEqual(result['BB McCullum'], 841)
+
 
 if __name__ == "__main__":
 
